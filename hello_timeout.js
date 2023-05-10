d65d9d19ed93455c4ba81047a189ccc8d10e80f6
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-09 18:21:47 -0700

    creating a repo

---
+++ hello_timeout.js
@@ -0,0 +1,13 @@
+// const print = () => {
+//   console.log("before 4311o th3r3 w0r1d")
+// }
+// print();
+
+setTimeout(() => {
+  console.log("4311o")
+}, 500); 
+setTimeout(() => {
+  console.log("h3r3")
+}, 1000); setTimeout(() => {
+  console.log("w0r1d")
+}, 1500); 
